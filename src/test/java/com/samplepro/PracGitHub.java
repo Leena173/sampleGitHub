8a3b429fc1574b1e41fe8cc022a0aa78d232d6a6
Welcome <Welcome@JESVIN-3011>
2022-08-24 15:23:17 +0530

    Updated script

--- src/test/java/com/samplepro/PracGitHub.java
+++ src/test/java/com/samplepro/PracGitHub.java
@@ -5,6 +5,8 @@ public class PracGitHub {
 	public static void main(String[] args)
 	{
        System.out.println("github is ready");
+       
+       System.out.println("github changes done");
 	}
 
 }
